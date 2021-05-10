9e1a6db96828650e49f3164433bc35a104f5cb0e
hardikphalet <hardik.phalet@gmail.com>
2021-05-10 12:46:37 +0530

    Made a menu

--- LearnGL-Cherno/src/Application.cpp
+++ LearnGL-Cherno/src/Application.cpp
@@ -22,6 +22,8 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 
+#include "tests/TestClearColor.h"
+
 const unsigned int SCR_WIDTH = 960;
 const unsigned int SCR_HEIGHT = 540;
 
@@ -61,116 +63,81 @@ int main(void)
     std::cout << "Cherno Engine v1.0\n" "[RELEASE MODE]\n\n";
 #endif // _DEBUG
 
-
     {
-        float positions[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f,
-             50.0f, -50.0f, 1.0f, 0.0f,
-             50.0f,  50.0f, 1.0f, 1.0f,
-            -50.0f,  50.0f, 0.0f, 1.0f
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+        float positions[6] = {
+            -0.5f, -0.5f,
+             0.5f, -0.5f,
+             0.5f,  0.5f,
         };
 
-        unsigned int indices[] = {
+
+        unsigned int indices[3] = {
+            0, 1, 2
+        };
+
+        float positions_glm[8] = {
+            100.0f, 100.0f,
+            200.0f, 100.0f,
+            200.0f, 200.0f,
+            100.0f, 200.0f
+        };
+
+        unsigned int indices_glm[6] = {
             0, 1, 2,
             2, 3, 0
         };
 
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-        IndexBuffer ib(indices, 6);
-
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -100.0f, 100.0f);
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-        
-        
-        
 
-        Shader shader("res/shaders/BasicProjection.shader");
+        VertexArray va;
+        VertexBuffer vb(positions_glm, 2 * 4 * sizeof(float));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
+        IndexBuffer ib(indices_glm, 6);
+
+        Shader shader("res/shaders/QuadProjection.shader");
         shader.Bind();
-        // shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-        
-
-        Texture texture("res/textures/Cherno.png");
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
 
         Renderer renderer;
         ImGui::CreateContext();
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translationA(200, 200, -50);
-        glm::vec3 translationB(400, 200, -50);
-        glm::vec3 translationC(600, 200, -50);
+        test::Test* currentTest = nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
 
-        float r = 0.0f;
-        float increment = 0.05f;
+        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
 
-        /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
-            /* Render here */
             renderer.Clear();
-
             ImGui_ImplGlfwGL3_NewFrame();
 
+            if (currentTest)
             {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationC);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            if (r > 1.0f)
-                increment = -0.05f;
-            else if (r < 0.0f)
-                increment = 0.05f;
-
-            r += increment;
-
-            {
-                static float f = 0.0f;
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-                ImGui::SliderFloat3("Translation C", &translationC.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                currentTest->OnUpdate(0.0f);
+                currentTest->OnRender();
+                ImGui::Begin("Test");
+                if (currentTest != testMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+                currentTest->OnImGuiRender();
+                ImGui::End();
             }
 
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
-            /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
-            /* Poll for and process events */
             glfwPollEvents();
         }
     }
